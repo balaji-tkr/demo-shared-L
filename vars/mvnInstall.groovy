48606412e77f82323c52218da66085ed632de581
Balaji <krbalajiram24@gmail.com>
2018-11-25 21:29:55 +0530

    Update mvnInstall.groovy

--- vars/mvnInstall.groovy
+++ vars/mvnInstall.groovy
@@ -1,13 +1,3 @@
 def call(String result){
-pipeline {
-    agent any
-    stages {
-        stage('Build') { 
-            steps {
-             sh 'mvn clean install'
-                
+            sh 'mvn clean install'
             }
-        }
-    }
- }
- }
