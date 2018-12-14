fe30112e00e5db80fff7bfd5829640be1aab65db
Balaji <krbalajiram24@gmail.com>
2018-12-14 17:20:35 +0530

    Update evenOrOdd.groovy

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -1,27 +1,8 @@
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
+               echo "The build number is even"
           }
-        }
+   else 
+     echo "The build number is odd"
       }
-    }
-  }
-}
-/*else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
-  }*/
 
