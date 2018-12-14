c99e86337c687c5592ecde5e326802a562a790f1
Balaji <krbalajiram24@gmail.com>
2018-12-14 17:23:17 +0530

    Update evenOrOdd.groovy

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -1,8 +1,11 @@
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
+    ansiColor('xterm') {
                echo "The build number is even"
+    }
           }
-   else 
+   else
+     ansiColor('xterm') {
      echo "The build number is odd"
       }
-
+}
